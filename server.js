7510e97fcdee7bfb3037e5bb39550006a5757e34
Naman <naman@techgropse.com>
2024-05-29 16:13:01 +0530

    done

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ mongoose
     "mongodb+srv://starStaging:nQ2zYmu1APv9r8Yh@cluster0.llrjflg.mongodb.net/feed?retryWrites=true&w=majority"
   )
   .then((con) => console.log("connected to remote database"));
-const port = 2083;
-process.env.BASE_URL = "http://localhost:2083/";
+const port = 3005;
+process.env.BASE_URL = "http://localhost:3005/";
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
