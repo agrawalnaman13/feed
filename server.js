770a6723379978d6145b23f73b65febbf49978d6
Naman <naman@techgropse.com>
2024-05-29 16:27:57 +0530

    done

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ mongoose
   )
   .then((con) => console.log("connected to remote database"));
 const port = 3005;
-process.env.BASE_URL = "http://ec2-52-66-186-107.ap-south-1.compute.amazonaws.com:3005/";
+process.env.BASE_URL =
+  "http://ec2-52-66-186-107.ap-south-1.compute.amazonaws.com:3005/";
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
